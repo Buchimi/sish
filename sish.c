30dc5e6c7d979b6ec18f123cea5900d209c162e9
Buchimi <stormzzy900@gmail.com>
2022-10-19 11:55:18 -0500

    mission successful

--- sish.c
+++ sish.c
@@ -3,20 +3,23 @@
 #include <error.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int getNoArgs(char *);
 int main(int argc, char *argv[])
 {
-    size_t bufferSize;
-    char *buffer;
+
     while (1)
     {
+        size_t bufferSize;
+        char *buffer;
         // get the string
         printf("sish> ");
         size_t characters = getline(&buffer, &bufferSize, stdin);
         //
-
         // tokenize the string
+        buffer[characters - 1] = '\0';
+
         int noOfPipes = 0;
         for (int i = 0; buffer[i] != '\0'; i++)
         {
@@ -33,13 +36,10 @@ int main(int argc, char *argv[])
             {
                 perror("pipe failed");
             }
-            else
-            {
-                printf("pipe successful\n");
-            }
         }
-        char *token;
-        char *saveptr;
+
+        char *token = NULL;
+        char *saveptr = NULL;
         char *delim = "|";
 
         for (int i = 0;; buffer = NULL, i++)
@@ -51,20 +51,19 @@ int main(int argc, char *argv[])
                 break;
             }
             int noArgs = getNoArgs(token);
-
             if (noArgs > 0)
             {
                 char *argu[noArgs + 1];
-                char *arguTok;
+                char *arguTok = NULL;
                 char *arguDelim = " ";
-                char *arguSavePtr;
+                char *arguSavePtr = NULL;
 
                 // tokenize and put in array
                 for (int j = 0;; token = NULL, j++)
                 {
-                    arguTok = __strtok_r(token, arguDelim, &arguSavePtr);
+                    arguTok = strtok_r(token, arguDelim, &arguSavePtr); //__strtok_r(token, arguDelim, &arguSavePtr);
                     argu[j] = arguTok;
-
+                    // printf("%s", arguTok);
                     if (arguTok == NULL)
                     {
                         break;
@@ -79,28 +78,60 @@ int main(int argc, char *argv[])
                 }
                 else if (cpid == 0)
                 {
-                    for (int j = 0; j < noOfPipes * 2; j++)
+                    for (int j = 0; j < (noOfPipes * 2); j++)
                     {
-                        if (j != (i * 2) || j != (i * 2 + 1))
+                        if (!(j == (i * 2 - 2) || j == (i * 2 + 1)))
                         {
-                            // close the pipes we don't use
+                            // i * 2 - 2 is the read pipe, i * 2 + 1 is the write pipe
+                            //  close the pipes we don't use
                             close(pipes[j]);
+                            printf("We closed pipe %d in cmd %s", j, argu[0]);
                         }
                     }
+
                     // child process
                     //  pipe
-
+                    if (i * 2 - 2 >= 0)
+                    {
+                        // read pipe
+                        // this won't run for the first cmd before the very first pipe. why?
+                        // it has an i == 0, thus a read fid at -2, which  is < 0
+                        dup2(pipes[i * 2 - 2], STDIN_FILENO);
+                        close(pipes[i * 2 - 2]);
+                    }
+                    if (i * 2 + 1 < noOfPipes * 2)
+                    {
+                        // say we have two pipes, i at the last pipe == 2.
+                        // thus this won't run for the very last cmd since it'll attempt to write beyond the array
+                        dup2(pipes[i * 2 + 1], STDOUT_FILENO);
+                        close(pipes[i * 2 + 1]);
+                    }
                     // execvp
+                    // printf("%s \n", argu[1]);
                     execvp(argu[0], argu);
                 }
-                else
+
+                // parent
+                // here we want to close the pipes we duped in child 2 ONLY
+
+                if (i * 2 - 2 >= 0)
                 {
-                    // parent
+                    // read pipe
+                    close(pipes[i * 2 - 2]);
                 }
+                if (i * 2 + 1 < noOfPipes * 2)
+                {
+                    // write pipe
+                    close(pipes[i * 2 + 1]);
+                }
+
+                // maybe we should wait for the child at this point
+                waitpid(cpid, NULL, 0);
             }
             // printf("%s \n", token);
             // printf("we have %d args in first pipe \n", noArgs);
         }
+
         // printf("%s", buffer);
         // printf("we have %d pipes \n", noOfPipes);
     }
